9d36a1aab8f7b5f80e09fff54b31230c072a46af
daksh2k <daksh2k@gmail.com>
2021-08-12 03:13:55 +0530

    Fix: Bug when tag is empty Add: Proper logging

--- retquote.py
+++ retquote.py
@@ -7,6 +7,12 @@ import datetime
 def current_time():
   return datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=5,minutes=30))).strftime('[%d/%b/%Y %I:%M:%S %p] ')
 
+#Print Quote With Proper Formatiing
+def print_quote(qt):
+  qt_list = qt.split('\n')
+  qt_list = [f"{current_time()}Line {i+1}: {line}" for i,line in enumerate(qt_list)]
+  return '\n'.join(qt_list)
+  
 #Update tweetcount in quotes DB
 def update_count(doc,coll):
     new_value = {'$inc' :{'tcount' : 1}}
@@ -15,7 +21,7 @@ def update_count(doc,coll):
 
 #Return the quote with proper formatting
 def parse_doc(doc):
-    tags_list = [tag.strip().replace(" ","").replace("-","_") for tag in doc["tags"].strip().split(',')]
+    tags_list = [tag.strip().replace(" ","").replace("-","_") for tag in doc["tags"].strip().split(',') if tag.strip().replace(" ","").replace("-","_")!=""]
     quote  = doc["text"].strip()
     auth = doc["author"].strip().split(',')[0]
     quote += "\n–"+auth.strip() 
@@ -26,23 +32,23 @@ def parse_doc(doc):
     except IndexError:
       pass
     tags_list = random.sample(tags_list,len(tags_list))
-    tags_list_f = []
+    tags_list_final = []
     for tag in tags_list:
-        if len(tags_list_f)==3:
+        if len(tags_list_final)==3:
             break
         if len(tag)<15:
-           tags_list_f.append(tag)    
-    if not tags_list_f:
+           tags_list_final.append(tag)    
+    if not tags_list_final:
         for tag in tags_list:
-          if len(tags_list_f)==2:
+          if len(tags_list_final)==2:
              break
-          tags_list_f.append(tag)
-    tags = "\n#quotes #"+" #".join(tags_list_f)
+          tags_list_final.append(tag)
+    tags = "\n#quotes #"+" #".join(tags_list_final)
     if tags[-2:]==" #":
         tags = tags[:-2]
     if len(quote+tags)<550:
       quote += tags
-    print(f"{current_time()}Returned quote from DB: {quote}")
+    print(f"{current_time()}Returned quote from DB-:\n{quote}")
     return quote
 
 #Get quote from a very large dataset(Around 500k) 
@@ -53,15 +59,15 @@ def main(client):
         try:
             quote = parse_doc(doc)
         except Exception as e:
-            print(f"{current_time()}Error parsing doc quote\n{e}")
+            print(f"{current_time()}Error parsing doc quote\n{current_time()}{e}")
             sleep(3)
             return main(client)     
         try: 
             update_count(doc,coll)
         except Exception as e:
-            print(f"{current_time()}Error in Updating tcount in db\n{e}")
+            print(f"{current_time()}Error in Updating tcount in db\n{current_time()}{e}")
     if len(quote)>560:
         sleep(3)
-        print(f"{current_time()}Very long quote! {len(quote)} words\n{quote}\nGetting again!")
+        print(f"{current_time()}Very long quote! {len(quote)} words\n{current_time()}Getting again!")
         return main(client)
     return quote  

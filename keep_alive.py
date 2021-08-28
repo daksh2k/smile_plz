e7b8d223d8fa2a9361352e801aa9b3570f82ff43
daksh2k <daksh2k@gmail.com>
2021-08-28 18:24:21 +0530

    Update: log display ui

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,13 @@
-from flask import Flask,render_template,abort,send_file,request,redirect
+from flask import Flask,render_template,abort,send_file,request
+from flask_compress import Compress
 from threading import Thread
-from concurrent.futures import as_completed, ThreadPoolExecutor
 import datetime
 import os
 import re
 import twitter
 
 app = Flask(__name__,template_folder='templates',static_folder='static')
-# app.debug = True
+Compress(app)
 
 # Parse the Log File to get Individual Tweet Sessions
 def calculate_sessions(log_file_lines):
@@ -28,7 +28,7 @@ def calculate_sessions(log_file_lines):
 # Return the List of all Log Files Present in the System
 def get_all():
   log_list = [re.search(r"log_(\d{2}_\d{2}_\d{2})",log_file).group(1).replace("_","/") for log_file in os.listdir('Logs')]
-  log_list.reverse()
+  log_list.sort(key = lambda date: datetime.datetime.strptime(date, '%d/%m/%y'),reverse=True)
   log_dict = {}
   for logf in log_list:
      log_file = open(f"Logs/log_{logf.replace('/','_')}.txt","r",encoding="utf-8")
@@ -48,15 +48,10 @@ def calculate_summary(fin_list):
             tweets_to_get.append(line.split(' ')[2].strip())
   tot_favs  = 0
   tot_rts   = 0
-  try:
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(api.get_status,tweetid) for tweetid in tweets_to_get]
-        for f in as_completed(futures):
-            tweet = f.result()
-            tot_favs  += tweet.favorite_count
-            tot_rts   += tweet.retweet_count
-  except Exception as e:
-      print(e)    
+  tweets = api.statuses_lookup(tweets_to_get,trim_user=True)
+  for tweet in tweets:
+    tot_favs  += tweet.favorite_count
+    tot_rts   += tweet.retweet_count  
   summary = {
      "Tweets Sent Today" : len(fin_list),
      "Favourites Today"  : tot_favs,
@@ -67,44 +62,39 @@ def calculate_summary(fin_list):
   }
   return summary
 
-# Main Route for showing twitter profile
+# Base Route for showing twitter profile
 @app.route('/',methods=['GET'])
 def main():
   return render_template("index.html",profile=os.environ.get("twitter_profile","https://twitter.com/smile_plz12"))
 
-#Show the latest Log file 
+# Show the latest Log file or a specified Log File
 @app.route('/log',methods=['GET'])
-def show_logs():
+@app.route('/log/<datelog>',methods=['GET'])
+def show_logs(datelog=None):
   log_date = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=5,minutes=30)))
-  try:
+  if datelog in (log_date.strftime('%d_%m_%y'),"latest") or datelog is None:
+    try:
       log_file = open(f"Logs/log_{log_date.strftime('%d_%m_%y')}.txt","r",encoding="utf-8")
       log_date_formatted = log_date.strftime('%d/%m/%y (Latest)') 
-  except FileNotFoundError:
+    except FileNotFoundError:
       log_file = open(f"Logs/log_{(log_date-datetime.timedelta(days=1)).strftime('%d_%m_%y')}.txt","r",encoding="utf-8")
       log_date_formatted = (log_date-datetime.timedelta(days=1)).strftime('%d/%m/%y (Previous Day)')
+  else:
+    if not os.path.isfile(f"Logs/log_{datelog}.txt"):
+        return abort(404)
+    log_file = open(f"Logs/log_{datelog}.txt","r",encoding="utf-8")
+    log_date_formatted = datelog.replace('_','/')+" (Old)"
   log_file_lines = log_file.readlines()
   log_file.close()
   final_list = calculate_sessions(log_file_lines)
   return render_template("log.html",log_list=final_list,log_date=log_date_formatted)
 
-#Show the Particular Log File According to the date
-@app.route('/log/<datelog>',methods=['GET'])
-def retlog(datelog):
-     if not os.path.isfile(f"Logs/log_{datelog}.txt"):
-        return abort(404)
-     log_file = open(f"Logs/log_{datelog}.txt","r",encoding="utf-8")
-     log_file_lines = log_file.readlines()
-     log_file.close()
-     final_list = calculate_sessions(log_file_lines)
-     return render_template("log.html",log_list=final_list,log_date=datelog.replace('_','/')+" (Old)") 
-
-#Main Route For Downloading Log Files
-@app.route('/download', defaults={'fname': None}, methods=['GET'])
-@app.route('/log/download',defaults={'fname': None},methods = ['GET'])
+# Main Route For Downloading Log Files
+@app.route('/log/download',methods = ['GET'])
 @app.route('/log/download/<path:fname>',methods=['GET'])
 def retfile(fname=None):
     latest_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=5,minutes=30)))
-    if fname in ("/","latest") or fname is None:
+    if fname == "latest" or fname is None:
       log_latest = f"Logs/log_{latest_time.strftime('%d_%m_%y')}.txt"
       if not os.path.isfile(log_latest):
           log_latest = f"Logs/log_{(latest_time-datetime.timedelta(days=1)).strftime('%d_%m_%y')}.txt"
@@ -112,11 +102,6 @@ def retfile(fname=None):
     if not os.path.isfile(f"Logs/log_{fname}.txt"):
         return abort(404)   
     return send_file(f"Logs/log_{fname}.txt",as_attachment=True)
-
-"""#Redirect to the main route for Downloading
-@app.route('/log/download',methods=['GET'])
-def download():
-    return redirect('/log/download/latest',code=303)"""
 
 # Async Route For Fetching Summary
 @app.route('/log_summary',methods=['GET'])
@@ -131,14 +116,15 @@ def get_summary():
 #Async Route for returning Log List
 @app.route('/log_all_list',methods=['GET'])
 def log_all_list():
-    return render_template("loglist.html",logs_all=get_all()) 
+  return render_template("loglist.html",logs_all=get_all()) 
     
 def run():
-  app.run(host=os.environ.get("host"), port=8080)
+  app.run(host=os.environ.get("host","0.0.0.0"), port=8080)
 
 def keep_alive():
   server = Thread(target=run)
   server.start()
 
 if __name__ == '__main__':
-    run()
+  app.debug = True
+  run()
--- static/main.js
+++ static/main.js
@@ -1,32 +1,128 @@
-  get_log_list()
-  get_summary()
-  function togglelist(){
-    var cntnr = $(".log-list-container")
-    var btn   = $(".log-list-show")
-    if(cntnr[0].style.display==="block"){
-      cntnr.hide()
-      btn[0].textContent = "Show All Logs"
-    }
-    else{
-      cntnr.show()
-      btn[0].textContent = "Hide Logs"
-    }
-  }
-  function get_summary(){
-    $.ajax({url: "/log_summary",
-           headers:{"logdate": $("h1").text()},
-           success: (res) => $(".row").prepend(res)
-         });
-    }
-  function get_log_list(){
-    $.ajax({url: "/log_all_list",
-           success: (res) => {
-            $(".log-list-container").append(res);
-            for (const file of $(".list-group-item")){
-              if(file.children[0].text.split(' ')[1] == $("h1").text().split(' ')[1]){
-                file.classList.add("active")
-              }
-            } 
+// Get summary of the todays tweets.
+(function get_summary(){
+  $.ajax({url: "/log_summary",
+         headers:{"logdate": $("h1").text()},
+         success: (res) => {
+           $(".row").prepend(res)
+           updateScrollButtons()
           }
-        });
+       });
+  })();
+
+// Get list of all logs present. 
+(function get_log_list(){
+  $.ajax({url: "/log_all_list",
+         success: (res) => {
+          $(".log-list-container").append(res);
+          $(".list-group-item").each( function (){
+            if(this.children[0].text.split(' ')[1] == $("h1").text().split(' ')[1]){
+              $(this).addClass("active")
+            }
+          })
+        }
+      });
+})();
+
+/* Add event listener for long press on arrow buttons
+Credits- https://github.com/john-doherty/long-press-event */
+$(".s-up").on('long-press',(evt) => {
+  evt.preventDefault() 
+  scrollToTop()
+});
+$(".s-down").on('long-press',(evt) => {
+  evt.preventDefault() 
+  scrollToBottom()
+});
+
+// Add Event Listener for right click on arrow buttons (easier on desktop)
+$(".s-up").on('contextmenu',(evt) => {
+  evt.preventDefault() 
+  scrollToTop()
+});
+$(".s-down").on('contextmenu', (evt) => {
+  evt.preventDefault() 
+  scrollToBottom()
+});
+
+// Hide scroll buttons according to scroll position
+$(document).on("scroll",updateScrollButtons)
+
+// Scroll to the top of the document
+function scrollToTop(){
+  $([document.documentElement, document.body]).animate({
+    scrollTop: 0
+  }, 800,"linear");
+}
+
+// Scroll to the bottom of the document
+function scrollToBottom(){
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $(document).height()
+  }, 800,"linear");
+}
+
+// Hiding scroll buttons when at the top or at the bottom
+function updateScrollButtons(){
+  const docHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+  const currLoc = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currLoc< 400)
+    $(".s-up").hide(200);
+  else
+    $(".s-up").show(200);
+  if(docHeight-currLoc < docHeight/15)
+    $(".s-down").hide(200);
+  else
+    $(".s-down").show(200);  
+}
+
+// For Showing the log list on button press
+function togglelist(){
+  var cntnr = $(".log-list-container")
+  var btn   = $(".log-list-show")
+  if(cntnr.css('display')==="block"){
+    cntnr.hide()
+    btn[0].textContent = "Show All Logs"
   }
+  else{
+    cntnr.show()
+    btn[0].textContent = "Hide Logs"
+  }
+}
+
+//  Scroll to the next session based on current session in viewport
+//  on arrow down button press 
+function scrollNext()  {
+  const topmin = [];
+  $(".cols").each( function() {
+    const rect = this.getBoundingClientRect();
+    topmin.push(Math.abs(rect.top))
+  })
+  var idno = topmin.indexOf(Math.min(...topmin))+1
+  var eleclass = ".ses-"+ idno
+  if ( idno < topmin.length ){
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $(eleclass).offset().top - 35
+    }, 250);
+  }
+  else
+    scrollToBottom()
+ }
+
+//  Scroll to the session before based on current session in viewport
+//  on arrow up button press 
+function scrollBefore(){
+  const topmin = [];
+  $(".cols").each( function() {
+    const rect = this.getBoundingClientRect();
+    topmin.push(Math.abs(rect.top))
+  })
+  var idno = topmin.indexOf(Math.min(...topmin))-1
+  var eleclass = ".ses-"+ idno
+  if(idno>=0){
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(eleclass).offset().top - 35
+    }, 250);
+  }
+  else
+    scrollToTop()
+}
